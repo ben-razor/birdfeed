e384b8270d5b69e3d3a927a871614705c641d87a
Ben Razor <chrisbenwar1@googlemail.com>
2021-05-12 00:22:59 +0100

    Access python feed api from react

--- web/src/App.js
+++ web/src/App.js
@@ -3,7 +3,7 @@ import {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
-  const [feeds, setFeeds] = useState(["A feed", "Another feed"]);
+  const [feeds, setFeeds] = useState([]);
 
   useEffect(() => {
     fetch("https://birdfeed-01000101.ew.r.appspot.com/api/feed", {
@@ -16,18 +16,50 @@ function App() {
       return response.json();
     }).then((json) => {
       console.log(json);
+      let prevSource = '';
+      let prevDateStr = '';
+
+      for(let feed of json) {
+
+        let date = new Date(feed['date']);
+        let dateStr = date.toLocaleDateString();
+        feed['time_str'] = date.toLocaleTimeString();
+
+        if(dateStr === prevDateStr) {
+          dateStr = '';
+        }
+        else {
+          prevDateStr = dateStr;
+        }
+        feed['date_time_str'] = dateStr;
+
+        let source = feed['source'];
+
+        if(source === prevSource) {
+          if(dateStr === '') source = '';
+        }
+        else {
+          prevSource = source;
+        }
+        feed['source'] = source;
+
+      }
       setFeeds(json); 
     });
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-          Bird Feeder
-      {feeds.map((value, index) => {
-        return <div>{value.title}</div>
+    <div>
+      <table class="fancy"> 
+      {feeds.map((feed, index) => {
+        return <tr>
+            <td class="date">{ feed.date_time_str }</td>
+            <td class="source">{ feed.source }</td>
+            <td class="time">{ feed.time_str }</td>
+            <td class="title"><a href="{{ feed.link }}" target="_blank">{ feed.title }</a></td>
+        </tr> 
       })}
-      </header>
+      </table>
     </div>
   );
 }
