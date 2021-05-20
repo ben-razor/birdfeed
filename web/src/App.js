f01d58a228d56beee813c8482176ecb3dec8d419
Ben Razor <chrisbenwar1@googlemail.com>
2021-05-20 08:24:38 +0100

    Refactor feed display to use components

--- web/src/App.js
+++ web/src/App.js
@@ -1,90 +1,9 @@
 import logo from './logo.svg';
 import {useEffect, useState} from 'react';
 import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import Feeds from "./components/feeds"
 import './App.css';
-
-/**
- * Returns time in format HH:MM:SS.
- * 
- * @param {string} date 
- * @param {boolean} useSeconds Whether to include seconds
- * @returns string
- */
-function formatTime(date, useSeconds=true) {
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
-
-  let timeStr = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
-
-  if(useSeconds) {
-    timeStr += `:${seconds.toString().padStart(2, '0')}`;
-  }
-
-  return timeStr;
-}
-
-/**
- * Converts a string to a colour with a given hue, 100% saturation and a value
- * from baseLevel to baseLevel + level.
- * 
- * It calculates the colour using the (mod % levels) of the sum of the character
- * codes in string. It is inefficient but fun.
- * 
- * @param {string} source 
- * @param {number} hue 
- * @param {number} levels 
- * @param {number} baseLevel 
- * @returns string
- */
-function stringToColour(text, hue, levels, baseLevel) {
-  let variant = text.split('').map(x => x.charCodeAt(0)).reduce((p, c) => p + c) % 20 + 15; 
-  let variantCSS = `hsl(190, 100%, ${variant}%)`;
-  return variantCSS;
-}
-
 function App() {
-  const [feeds, setFeeds] = useState([]);
-
-  useEffect(() => {
-    fetch("https://birdfeed-01000101.ew.r.appspot.com/api/feed", {
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }).then(response => response.json()).then(json => {
-      let prevSource = '';
-      let prevDateStr = '';
-
-      for(let feed of json) {
-        let source = feed['source'];
-        let date = new Date(feed['date']);
-        let dateStr = date.toLocaleDateString();
-        feed['time_str'] = formatTime(date, false);
-        date.toDateString()
-
-        if(dateStr === prevDateStr) {
-          dateStr = '';
-        }
-        else {
-          prevDateStr = dateStr;
-        }
-        feed['date_time_str'] = dateStr;
-
-        feed['color'] = stringToColour(source)
-
-        if(source === prevSource) {
-          if(dateStr === '') source = '';
-        }
-        else {
-          prevSource = source;
-        }
-        feed['source'] = source;
-
-      }
-      setFeeds(json); 
-    });
-  }, [])
 
   return (
     <Router>
@@ -102,18 +21,7 @@ function App() {
         <Route path="/setup">
         </Route>
         <Route path="/">
-          <div>
-            <table class="fancy"> 
-            {feeds.map((feed, index) => {
-              return <tr>
-                  <td class="date">{ feed.date_time_str }</td>
-                  <td class="source" style={{"color": "white", "background-color": feed.color}}>{ feed.source }</td>
-                  <td class="time">{ feed.time_str }</td>
-                  <td class="title"><a href={ feed.link } target="_blank">{ feed.title }</a></td>
-              </tr> 
-            })}
-            </table>
-          </div>
+          <Feeds />
         </Route>
       </Switch>
     </Router>
