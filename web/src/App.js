06219b4cec33c96a30759d38565639402b13b606
Ben Razor <chrisbenwar1@googlemail.com>
2021-05-20 10:19:05 +0100

    Create page header

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import {useEffect, useState} from 'react';
 import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
-import Feeds from "./components/feeds"
-import FeedSetup from "./components/feed_setup"
+import Feeds from './components/feeds';
+import FeedSetup from './components/feed_setup';
+import FeedForm from './components/feed_form';
 import './App.css';
 function App() {
 
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <NavLink activeClassName="active" exact="true" to="/">Latest</NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="active" to="/setup">Setup</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <header>
+        <h2 class="logo">Birdfeed</h2>
+        <nav>
+          <ul>
+            <li>
+              <NavLink activeClassName="active" exact="true" to="/">Latest</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="active" to="/setup">Setup</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+      </header>
       <Switch>
         <Route path="/setup">
+          <FeedForm />
           <FeedSetup />
         </Route>
         <Route path="/">
